e97ab610da86e2bd1b0e739d139cc010ba2ae891
venubabukandimalla <venubabukandimalla@192.168.0.2>
2015-12-20 10:18:09 -0700

    Initial Tool01 checkin.

---
+++ Tool01/src/Tool01Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author venubabukandimalla
+ *
+ */
+public class Tool01Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		System.out.println(new Tool01Main().add(2, 4));
+	}
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+
+}
